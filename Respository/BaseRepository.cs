6127be4e2f4dee09f5e0b97d35a646fac0738c38
ramyTawfik <rtawfik@red.sa>
2023-04-23 15:35:44 +0200

    new

--- Respository/BaseRepository.cs
+++ Respository/BaseRepository.cs
@@ -42,7 +42,7 @@ namespace OnionArchitecture.EF.Repositories
                     query = query.Include(include);
                 }
             }
-            return query.SingleOrDefault(match);
+            return query.AsNoTracking().SingleOrDefault(match);
         }
 
         public IEnumerable<T> FindAll(Expression<Func<T, bool>> match, string[] includes = null)
@@ -55,12 +55,12 @@ namespace OnionArchitecture.EF.Repositories
                     query = query.Include(include);
                 }
             }
-            return query.Where(match).ToList();
+            return query.AsNoTracking().Where(match).ToList();
         }
 
         public IEnumerable<T> FindAll(Expression<Func<T, bool>> match, int take, int skip)
         {
-            return _entities.Where(match).Take(take).ToList();
+            return _entities.AsNoTracking().Where(match).Take(take).ToList();
         }
 
         public IEnumerable<T> FindAll(Expression<Func<T, bool>> match, int? take, int? skip, Expression<Func<T, object>> orderBy = null, string orderByDirection = "Ascending")
@@ -87,27 +87,27 @@ namespace OnionArchitecture.EF.Repositories
                     query = query.OrderByDescending(orderBy);
                 }
             }
-            return query.ToList();
+            return query.AsNoTracking().ToList();
         }
 
         public IEnumerable<T> GetAll()
         {
-            return _context.Set<T>().ToList();
+            return _context.Set<T>().AsNoTracking().ToList();
         }
 
         public async Task<IEnumerable<T>> GetAllAsync()
         {
-            return await _context.Set<T>().ToListAsync();
+            return await _context.Set<T>().AsNoTracking().ToListAsync();
         }
 
         public T GetById(int id)
         {
-            return _context.Set<T>().Find(id);
+            return _context.Set<T>().AsNoTracking().Find(id);
         }
 
         public async Task<T> GetByIdAsync(int id)
         {
-            return await _context.Set<T>().FindAsync(id);
+            return await _context.Set<T>().AsNoTracking().FindAsync(id);
         }
 
         public T Update(T entity)
@@ -142,17 +142,17 @@ namespace OnionArchitecture.EF.Repositories
 
         public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
         {
-            return await ApplySpecification(spec).FirstOrDefaultAsync();
+            return await ApplySpecification(spec).AsNoTracking().FirstOrDefaultAsync();
         }
 
         public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
         {
-            return await ApplySpecification(spec).ToListAsync();
+            return await ApplySpecification(spec).AsNoTracking().ToListAsync();
         }
 
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
-            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(),spec);
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable().AsNoTracking(), spec);
         }
     }
 }
